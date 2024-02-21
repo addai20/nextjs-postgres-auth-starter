578fd5c5f00421e9ac0e96e804e299af5cfee772
Benjamin Addai <37193564+addai20@users.noreply.github.com>
2024-02-20 21:20:13 -0300

    Revert "Ba update login page (#2)" (#3)
    This reverts commit 045de48051b3135e3926f0a781ed84b52c99a53b.

--- components/header.tsx
+++
@@ -1,8 +0,0 @@
-export default function HeaderNav() { 
-  return(
-    <div className="shadow-md w-full fixed top-0 left-0">
-      <div className="md:flex bg-white py-4"></div>
-      <div>Hello</div>
-    </div>
-  )
-}
